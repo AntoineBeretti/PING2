b79b3dd9979ffa30f3a20db5d429a72ae8b07600
AntoineBeretti <113696014+AntoineBeretti@users.noreply.github.com>
2025-01-06 17:43:58 +0200

    Ecriture de la classe Gamemode, Input et Output

--- src/classes/gameMode.py
+++ src/classes/gameMode.py
@@ -1,10 +1,34 @@
-class GameMode:
+from abc import ABC, abstractmethod
+from gameController import 
+
+
+
+class Input:
+    def __init__(self, gameController=false, ballPose=0, playerPose):
+        self.gameController=gameController
+        self.ballPose=ballPose
+        self.playerPose=playerPose
+
+class Output:
+    def __init__(self, led=0, sound=0, playerMove=False):
+        self.led=led
+        self.sound=sound
+        self.playerMove=playerMove
+
+class GameMode(ABC):
     """
     Mother class to all game mode that Should be inherited.
     Every game mode describe the behavior of outputs (LEDs, sound, and player moves) 
     based on the inputs (game controller, ball pose, and player pose). additional, it take UI panel as settings.
     """
-    
+    @abstractmethod
+    def run(self,Input):
+        pass
+
+    @abstractmethod
+    def stop(self,Input):
+        pass
+
     pass
 
 class RedLightGreenLight(GameMode):
@@ -26,6 +50,8 @@ class LightTracker(GameMode):
     Game mode of Light Tracker. Traque-lumière in french.
     """
     
+    self.
+
     pass
 
 class MemoChain(GameMode):
