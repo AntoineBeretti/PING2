6fed3af666a0062802c1296a6b55ab45d162152f
Antoine SOUXDORF <antoine.souxdorf@student.junia.com>
2025-01-10 16:41:02 +0100

    Ajout d'une documentation pour la classe Input, décrivant les entrées disponibles et leur utilisation.

--- src/classes/input/input.py
+++ src/classes/input/input.py
@@ -1,11 +1,15 @@
 from abc import ABC, abstractmethod
+
+"""
+Mother class to all inputs 
+Inputs : PlayerInput (linearActuatorInput, BeamSwitch...), GameController3button
+"""
+
 class Input:
     def __init__(self, ListPlayerInput, OtherInput):
         self.ListPlayerInput=ListPlayerInput
         self.OtherInput=OtherInput
     pass
-
-
 
 
 
